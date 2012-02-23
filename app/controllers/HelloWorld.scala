6e7e27993c417d3bd43ffc58b4d88fecf7c27319
Hendrik Schnepel <hendrik.schnepel@gmail.com>
2012-02-23 20:21:48 +0100

    fixed that hack

--- app/controllers/HelloWorld.scala
+++ app/controllers/HelloWorld.scala
@@ -5,9 +5,6 @@ import play.api.mvc.{ Controller, Action }
 
 object HelloWorld extends Controller {
   def helloPlay(name: String) = Action {
-    Visitor.add("Java")
-    Visitor.add("Play Framework")
-    Visitor.add("Scala")
     val recentNames = Visitor.recentNames()
     Visitor.add(name)
     Ok(views.html.simple(name, recentNames))
